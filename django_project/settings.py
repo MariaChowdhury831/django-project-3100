5ffe54160fef3c75a7486885020d30a0c86a8bad
MariaChowdhury831 <89926974+MariaChowdhury831@users.noreply.github.com>
2021-11-25 02:26:55 +0600

    Debug Update

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-79zrrpg=23p6vi%6cqwa@7_niic55nlkght-kov6pr3$jnx8aq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['infinite-savannah-56335.herokuapp.com','127.0.0.1']
 
